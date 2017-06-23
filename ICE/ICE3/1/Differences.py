0cd0871fefdc6d582f24b0789f57fe09ded6d324
saijyothi9 <saijyothigudibandi9@gmail.com>
2017-06-23 12:10:38 -0500

    Delete Differences.py

--- ICE/ICE3/1/Differences.py
+++
@@ -1,4 +0,0 @@
-print('The key Differeces between between python2 and 3 versions:')
-print('1.Print Function: In python2 no need to use the ()-parantheses to print the statements, In python3 python2 print statement s replaced by print()')
-print('2.Unicode: str type in python2 is ASCII code, but in python3 is Unicode')
-print('3.EXCEPTIONS: Python3 requires different syntax for raising exceptions, python 3s syntax can be executed in python2 but the python3s syntax cannot be executed in python3')
